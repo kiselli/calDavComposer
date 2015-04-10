d33e49123031c44d416a6d70ca7dcbc29fd79971
Oliver Buchmann <obuchmann@pc-web.at>
2015-04-10 09:41:27 +0200

    Demo hinzugefügt

--- demo/config.js
+++ demo/config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	port: 1337,
+	allowSelfSignedCertificate : true,
 	calendars: [
-		'https://cis.technikum-wien.at/webdav/google.php?cal=SqE4PWs1j38s_2U1fHx44jhAAojlDzzB_KJvLpRfgVA&1410365527.1084'
+		'https://openServer/icsLocation',
+		'https://user:password@securedServer/icsLocation'
 	]
 }
--- demo/server.js
+++ demo/server.js
@@ -3,14 +3,21 @@ var url = require('url')
 var composer = require('../')
 var config = require('./config')
 
+if(config.allowSelfSignedCertificate){
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+}
+
 var baseDirectory = __dirname   // or whatever base directory you want
 
 http.createServer(function (request, response) {
-   var requestUrl = url.parse(request.url)
-   var fsPath = baseDirectory+requestUrl.pathname
-
-   composer.composeFromUrls(config.calendars).then(function(calendar){
-   	response.writeHead(200, {'Content-Type': 'text/calendar'})
-   	response.end(calendar);
-   })
+	try{
+	   composer.composeFromUrls(config.calendars).then(function(calendar){
+	   	response.writeHead(200, {'Content-Type': 'text/calendar'})
+	   	response.end(calendar);
+	   })
+	}catch(e){
+		response.writeHead(500);
+		response.end();
+	}
 }).listen(config.port)
+console.log("CalDav Demo listening on port "+config.port)
